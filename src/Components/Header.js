bef22564f7dadb3a8e076ea6b0b017a4914a1b47
Neeraj Kumar <neeraj@edfora.com>
2021-12-20 02:35:01 +0530

    UI fixws for smaller devices

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,10 +14,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const coinOptions = [
-  { value: "polkadot", label: (<>
-  <span>Polkadot</span>
-  {/* <img src={} /> */}
-  </>) },
+  {
+    value: "polkadot",
+    label: (
+      <>
+        <span>Polkadot</span>
+        {/* <img src={} /> */}
+      </>
+    ),
+  },
   { value: "kusama", label: "Kusama" },
 ];
 
@@ -85,7 +90,9 @@ function Header() {
     <>
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="#home" className="logo">Kusama <span>calculator</span> </Navbar.Brand>
+          <Navbar.Brand href="#home" className="logo">
+            Kusama <span>calculator</span>{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="customNav">
             <Nav className="">
@@ -99,67 +106,68 @@ function Header() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container className="calContainer">
-        {/* <nav className="flex items">
+      <div className="calContainer">
+        <Container>
+          {/* <nav className="flex items">
           <div className="left">
             <a href="/">
               <span>Logo</span>
             </a>
           </div> */}
-        {/* <Nav className="customNav">
+          {/* <Nav className="customNav">
             <Nav.Link href="/Price">Price</Nav.Link>
             <Nav.Link href="/EMA30">EMA30</Nav.Link>
             <Nav.Link href="/Stakes">Stakes</Nav.Link>
             <Nav.Link href="/About">About</Nav.Link>
           </Nav> */}
-        {/* </nav> */}
-        <Row>
-          <Col md={6}>
-            <div className="imgBlock">
-              <img src="./money-img.png" alt="" width="100%" />
-            </div>
-          </Col>
+          {/* </nav> */}
+          <Row>
+            <Col md={6}>
+              <div className="imgBlock">
+                <img src="./money-img.png" alt="" width="100%" />
+              </div>
+            </Col>
 
-          <Col md={6}>
-            {/* <h1>Choose Your Currency </h1> */}
-            <div className="customCard">
-              <div className="inputContainer">
-                <div>
-                  {/* <label htmlFor="kusama"></label> */}
-                  <input
-                    type="text"
-                    name="kusama"
-                    value={coinId}
-                    onChange={(e) => setCoinId(e.target.value)}
-                    id="kusama"
-                    placeholder="1.00"
-                  />
+            <Col md={6}>
+              {/* <h1>Choose Your Currency </h1> */}
+              <div className="customCard">
+                <div className="inputContainer">
+                  <div>
+                    {/* <label htmlFor="kusama"></label> */}
+                    <input
+                      type="text"
+                      name="kusama"
+                      value={coinId}
+                      onChange={(e) => setCoinId(e.target.value)}
+                      id="kusama"
+                      placeholder="1.00"
+                    />
+                  </div>
+
+                  <div>
+                    <Select
+                      // defaultValue={{ label: "Dot", value: 0 }}
+                      options={coinOptions}
+                      onChange={(item) => setSelectedCrypto(item.value)}
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <Select
-                    // defaultValue={{ label: "Dot", value: 0 }}
-                    options={coinOptions}
-                    onChange={(item) => setSelectedCrypto(item.value)}
-                  />
-                </div>
-              </div>
-
-              {/* <CustomSelect /> */}
-              {/* text box */}
-              <div className="inputContainer">
-                <div>
-                {/* <label htmlFor="inr">{selectedFiat}</label> */}
-                <input
-                  type="text"
-                  contentEditable={false}
-                  name="currency"
-                  value={coinId * price || 0}
-                  id="inr"
-                />
-              </div>
-              {/* <CurrencyConverter/> */}
-              {/* <div className="switch">
+                {/* <CustomSelect /> */}
+                {/* text box */}
+                <div className="inputContainer">
+                  <div>
+                    {/* <label htmlFor="inr">{selectedFiat}</label> */}
+                    <input
+                      type="text"
+                      contentEditable={false}
+                      name="currency"
+                      value={coinId * price || 0}
+                      id="inr"
+                    />
+                  </div>
+                  {/* <CurrencyConverter/> */}
+                  {/* <div className="switch">
                 <HiSwitchHorizontal
 								size="30px"
 								onClick={() => {
@@ -168,35 +176,36 @@ function Header() {
 							/>
               </div> */}
 
-              <div>
-                <Select
-                  options={countries.map((item) => ({
-                    value: item.currency.code,
-                    label: `${item.currency.code} - ${item.currency.name}` ,
-                  }))}
-                  onChange={(option) => setSelectedFiat(option.value)}
-                  // defaultValue={{ label: "Dollar", value: 0 }}
-
-                />
+                  <div>
+                    <Select
+                      options={countries.map((item) => ({
+                        value: item.currency.code,
+                        label: `${item.currency.code} - ${item.currency.name}`,
+                      }))}
+                      onChange={(option) => setSelectedFiat(option.value)}
+                      // defaultValue={{ label: "Dollar", value: 0 }}
+                    />
+                  </div>
+                </div>
+                <div className="text-center">
+                  <a className="currencyDisplay">
+                    {coinId} {selectedCrypto} ={" "}
+                    {
+                      countries.filter(
+                        (item) =>
+                          item.currency.code === selectedFiat.toUpperCase()
+                      )[0].currency.symbol
+                    }
+                    {/* {price} */}
+                    {coinId * price || 0}
+                  </a>
+                  {/* <button className="convert">Convert</button> */}
+                </div>
               </div>
-              </div>
-              <div className="text-center">
-              <a className="currencyDisplay">
-                {coinId} {selectedCrypto} ={" "}
-                {
-                  countries.filter(
-                    (item) => item.currency.code === selectedFiat.toUpperCase()
-                  )[0].currency.symbol
-                }
-                {/* {price} */}
-                {coinId * price || 0}
-              </a>
-                {/* <button className="convert">Convert</button> */}
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
